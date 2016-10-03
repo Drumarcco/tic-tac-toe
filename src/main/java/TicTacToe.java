f03af3771b8fa3b7178143a490259d0656b8a59b
Marco Alvarado <marcoantonioam9@gmail.com>
2016-10-02 21:36:54 -0700

    Adds CPU priority to center position and corners.

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -8,6 +8,7 @@ public class TicTacToe {
     private Player cpu;
     private int currentTurn = 0;
     private boolean humanStarted;
+    private final int BEST_POSITION = 5;
     private final String[] WINNING_CASES = new String[] {
         "123",
         "456",
@@ -57,14 +58,81 @@ public class TicTacToe {
     }
 
     private int getOptimalPosition(int lastHumanMovement) {
+        if (isMovementPossible(BEST_POSITION)) {
+            return BEST_POSITION;
+        }
+
+        int winningMovement = getWinningMovement();
+
+        if (winningMovement > 0) {
+            return winningMovement;
+        }
+
         int blockingMovement = getBlockingMovement(lastHumanMovement);
 
         if (blockingMovement > 0) {
             return blockingMovement;
         }
 
+        int corner = getCornerPosition(lastHumanMovement);
+
+        if (corner > 0) {
+            return corner;
+        }
+
+
         Random random = new Random();
         return random.nextInt(10) + 1;
+    }
+
+    private int getCornerPosition(int lastHumanMovement) {
+        int cornerOpposite = getCornetOpposite(lastHumanMovement);
+        if (cornerOpposite > 0) return cornerOpposite;
+
+        if (isMovementPossible(1)) return 1;
+        if (isMovementPossible(3)) return 3;
+        if (isMovementPossible(7)) return 7;
+        if (isMovementPossible(9)) return 9;
+
+        return 0;
+    }
+
+    private int getCornetOpposite(int lastHumanMovement) {
+        int corner = 0;
+        if (lastHumanMovement == 1) corner = 9;
+        else if (lastHumanMovement == 3) corner = 7;
+        else if (lastHumanMovement == 7) corner = 3;
+        else if (lastHumanMovement == 9) corner = 1;
+
+        if (!isMovementPossible(corner)) corner = 0;
+        return corner;
+    }
+
+    private int getWinningMovement() {
+        if (cpu.countMovements() < 2) return 0;
+
+        for (String winningCase : WINNING_CASES) {
+            int possibleWinningPosition = 0;
+            int matchingPositionsCount = 0;
+
+            for (String positionString : winningCase.split("")) {
+                int position = Integer.valueOf(positionString);
+
+                if (cpu.hasPosition(position)) {
+                    matchingPositionsCount++;
+                } else {
+                    if (isMovementPossible(position)) {
+                        possibleWinningPosition = position;
+                    }
+                }
+
+                if (matchingPositionsCount == 2 && possibleWinningPosition != 0) {
+                    return possibleWinningPosition;
+                }
+            }
+        }
+
+        return 0;
     }
 
     private int getBlockingMovement(int lastHumanMovement) {
